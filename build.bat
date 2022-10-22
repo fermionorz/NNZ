c392db25d469c057323fe36b16648e963ff99e76
Zer0-hex <Zer0-hex@outlook.com>
2022-10-23 05:01:24 +0800

    open source code

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 @pyinstaller -F -w -i res\images\logo.png --distpath . nnz.py
-@echo "PS C:\> certutil -hashfile nnz.exe [md5, SHA1, SHA256]" > Verification.txt
-@certutil -hashfile dist/nnz.exe md5 | findstr /V CertUtil >> Verification.txt
-@certutil -hashfile dist/nnz.exe sha1 | findstr /V CertUtil >> Verification.txt
-@certutil -hashfile dist/nnz.exe sha256 | findstr /V CertUtil >> Verification.txt
+@echo PS C:\> certutil -hashfile nnz.exe [md5, SHA1, SHA256] > Verification.txt
+@echo -------------------------------------------------------------------------------- >> Verification.txt
+@certutil -hashfile nnz.exe md5 >> Verification.txt
+@echo -------------------------------------------------------------------------------- >> Verification.txt
+@certutil -hashfile nnz.exe sha1 >> Verification.txt
+@echo -------------------------------------------------------------------------------- >> Verification.txt
+@certutil -hashfile nnz.exe sha256 >> Verification.txt
+@echo -------------------------------------------------------------------------------- >> Verification.txt
